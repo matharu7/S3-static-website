ae81c3dbc47fcc44471a3a3a5430895307c9a9f1
matharu7 <matharu7777@gmail.com>
2026-04-26 09:44:46 +0530

    Create script.js

---
+++ src/script.js
@@ -0,0 +1,6 @@
+function checkSystem() {
+    const status = document.getElementById('status');
+    status.innerText = "System Secure - No Threats Found";
+    status.style.color = "#3fb950";
+    alert("Scan Complete!");
+}
